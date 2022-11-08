357a0a828af86b1449d32916a0fef4980979a94b
songbetter <5ongnaeu17@gmail.com>
2022-11-09 00:25:25 +0900

    chore: QueryClientProvider 적용 - suspense와 useErrorBoundary 허용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import IntlWrapper from './lang/IntlWrapper'
 
 import './App.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      useErrorBoundary: true,
+    },
+  },
+})
+
 function App() {
-  return <IntlWrapper></IntlWrapper>
+  return (
+    <IntlWrapper>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
+    </IntlWrapper>
+  )
 }
 
 export default App
