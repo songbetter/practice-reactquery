b512f8a6c374c249f3610b63e4956bc7537ef293
songbetter <5ongnaeu17@gmail.com>
2022-11-14 18:26:36 +0900

    test: defineMessages 사용해보기 - link 내부에 있는 글자 번역 하는 법

---
+++ src/components/common/Footer.js
@@ -0,0 +1,31 @@
+import { defineMessages, FormattedMessage } from 'react-intl'
+
+const Footer = () => {
+  const link = (
+    <a href="/" className="text-orange">
+      <FormattedMessage {...messeges.cs} />
+    </a>
+  )
+  return (
+    <footer className="fixed bottom-4 left-4">
+      <div className="flex-col">
+        <div className="text-lg">Contact us</div>
+        <div className="text-base">
+          <FormattedMessage {...messeges.contactus} values={{ link }} />
+        </div>
+      </div>
+    </footer>
+  )
+}
+export default Footer
+
+const messeges = defineMessages({
+  cs: {
+    id: 'footer.cs',
+    defaultMessage: '"고객센터"',
+  },
+  contactus: {
+    id: 'footer_contactus',
+    defaultMessage: '카카오 플러스 친구 {link}에서 상담(10:00-18:00)',
+  },
+})
